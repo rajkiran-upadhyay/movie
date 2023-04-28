7d9af47f524010d59d2c43dc4d4d64fe127ca82a
raj <rk7889666@gmail.com>
2023-04-28 10:53:21 +0530

    first

---
+++ src/Movies.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+//import {AppContext} from './context'; vs
+import { useGlobalContext } from './context'
+
+const Movies = () => {
+  //const { movie, isLoading } = useGlobalContext(AppContext); vs
+  const { movie, isLoading } = useGlobalContext();
+
+  // console.log("jj", movie)
+  if (isLoading) {
+    return (
+      <div className=''>
+        <div className='loading'>
+
+          loading...
+        </div>
+
+
+      </div>
+    )
+  }
+  return (
+
+    <section className='movie-page'>
+
+      <div className='container grid grid-4-col'>
+        {movie.map((curMovie) => {
+          const { imdbID, Title, Poster } = curMovie;
+          const movieName = Title.substring(0, 15);
+          return <NavLink to={`movie/${imdbID}`} key={imdbID}>
+            <div className='card'>
+              <div className='card-info'>
+                <h2>{movieName.length >= 15 ? `${movieName}...` : movieName}</h2>
+                <img src={Poster} alt={imdbID} />
+              </div>
+            </div>
+          </NavLink>
+        })} </div>
+    </section>
+  );
+};
+export default Movies
